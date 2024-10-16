599618377c13d160e0ed4c6d90d58846b883b23a
hemil0102 <hemil0102@gmail.com>
2024-10-16 22:36:27 +0900

    xcode 프로젝트 생성 #2

---
+++ GumamdongAR/GumamdongARApp.swift
@@ -0,0 +1,17 @@
+//
+//  GumamdongARApp.swift
+//  GumamdongAR
+//
+//  Created by YEHROEI HO on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GumamdongARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
