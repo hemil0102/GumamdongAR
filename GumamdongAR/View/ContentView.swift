f649d1a149ebb4d768540dd18c1bf5ec72765cc4
hemil0102 <hemil0102@gmail.com>
2024-10-21 23:33:56 +0900

    feature: 증강 버튼 추가 #8

--- GumamdongAR/View/ContentView.swift
+++ GumamdongAR/View/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ARViewContainer()
-            .ignoresSafeArea()
+        VStack {
+            ARViewContainer()
+                .ignoresSafeArea()
+            
+            Button(action: {
+                print("버튼 눌림")
+            }, label: {
+                Text("증강하기")
+                    .frame(maxHeight: 60)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.yellow)
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
+                    .padding()
+            })
+
+        }
     }
 }
 
